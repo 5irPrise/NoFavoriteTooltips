aeab404df46ecf8891ed078c0dbc1fb672d08801
5irPrise <54083152+5irPrise@users.noreply.github.com>
2019-08-13 22:08:41 +0100

    Add files via upload

---
+++ NoFavoriteTooltips.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NoFavoriteTooltips
+{
+    public class NoFavoriteTooltips : Mod
+    {
+        public class NoFavoriteTooltipsGlobalItem : GlobalItem
+        {
+            public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+            {
+                tooltips.RemoveAll(line => line.mod == "Terraria" && line.Name.StartsWith("Favorite"));
+            }
+        }
+    }
+}
---
+++ description.txt
@@ -0,0 +1,18 @@
+[c/97FFFF:Description]
+It's a simple mod it does what its name implies, it removes the Marked as favorite tooltip.
+
+[c/97FFFF:Before]
+[c/FF0000:Meowmere]
+Marked as favorite
+Quick trash, stacking, and selling will be blocked
+200 melee damage
+4% critical strike chance
+Very Fast speed
+Strong knockback
+
+[c/97FFFF:After]
+[c/FF0000:Meowmere]
+200 melee damage
+4% critical strike chance
+Very Fast speed
+Strong knockback
